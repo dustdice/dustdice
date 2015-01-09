a1ec06b5f1daadb498fec2701395d2de21c16d6f
Rakesh <rakesh@moneypot.com>
2015-01-09 12:02:40 -0600

    add jquery ui

--- scripts/components/graph.js
+++ scripts/components/graph.js
@@ -1,236 +1,1 @@
-define([
-    'lib/react',
-    'highcharts-theme',
-    'game-logic/engine'
-],function(
-    React,
-    HighCharts,
-    Engine
-){
-    var D = React.DOM;
-
-    var MAX_NUM_POINTS = 30;
-
-    function getBetValues() {
-        return Engine.getBetValues();
-    }
-
-    return React.createClass({
-        displayName: 'Graph',
-
-        chart: null,
-
-        onWagerChange: function(wagerData) {
-            if(!this.isMounted())
-                return;
-
-            var chartLength = this.chart.series[2].data.length-1;
-            var chartIndex = this.chart.series[2].data[this.chart.series[2].data.length - 1].x;
-            this.updateProjections(chartLength, chartIndex, wagerData);
-        },
-
-        shouldComponentUpdate: function(nextProps, nextState){
-            return false; //Never render
-        },
-
-        onNewBet: function(game, wagerData) { //The position in the array of this bet
-            if(!this.isMounted())
-                return;
-
-            //Create the new point and add it without redrawing
-            var newPoint;
-            if(game.win)
-                newPoint = {
-                    color: 'green',
-                    lineColor: 'green',
-                    y: wagerData.gameProfit,
-                    radius: 7
-                };
-            else
-                newPoint = {
-                    color: 'red',
-                    y: wagerData.gameProfit,
-                    radius: 7
-                };
-
-            var chartLength = this.chart.series[2].data.length; //The real length of the chart
-            var chartIndex = this.chart.series[2].data[this.chart.series[2].data.length - 1].x + 1;
-
-            this.chart.series[2].data[chartLength-1].update({ radius: 4 }, false);
-            this.chart.series[2].addPoint(newPoint); //The length of the graph is not updated until render time
-
-            if(chartLength > MAX_NUM_POINTS)
-                this.chart.series[2].data[0].remove(false);
-
-            this.updateProjections(chartLength, chartIndex, wagerData, game);
-        },
-
-        updateProjections: function(chartLength, chartIndex, wagerData, game) {
-            var wonProjection = wagerData.gameProfit + (wagerData.wager * wagerData.payout) - wagerData.wager;
-            var LostProjection = wagerData.gameProfit - wagerData.wager;
-
-            if(game) {
-                if(game.win) {
-                    this.chart.series[0].data[0].remove(false);
-                    this.chart.series[0].addPoint([chartIndex + 1, wonProjection], false);
-
-                    this.chart.series[1].data[0].update([chartIndex, wagerData.gameProfit], false);
-                    this.chart.series[1].data[1].update([chartIndex + 1, LostProjection], false);
-                } else {
-                    this.chart.series[0].data[0].update([chartIndex, wagerData.gameProfit], false);
-                    this.chart.series[0].data[1].update([chartIndex + 1, wonProjection], false);
-
-                    this.chart.series[1].data[0].remove(false);
-                    this.chart.series[1].addPoint([chartIndex + 1, LostProjection], false);
-                }
-            } else {
-                this.chart.series[0].data[0].update([chartIndex, wagerData.gameProfit], false);
-                this.chart.series[0].data[1].update([chartIndex + 1, wonProjection], false);
-
-                this.chart.series[1].data[0].update([chartIndex, wagerData.gameProfit], false);
-                this.chart.series[1].data[1].update([chartIndex + 1, LostProjection], false);
-            }
-
-            this.chart.redraw();
-        },
-
-        componentDidMount: function() {
-            Engine.addWagerListener(this.onWagerChange);
-            Engine.addBetListener(this.onNewBet);
-            this.chart = createChart(this.refs.graphBox.getDOMNode(), Engine.getWagerValues());
-        },
-
-        componentWillUnmount: function() {
-            Engine.removeWagerListener(this.onWagerChange);
-            Engine.removeBetListener(this.onNewBet);
-
-            this.chart.destroy();
-        },
-
-        render: function() {
-            return D.div({ ref: 'graphBox' });
-        }
-    });
-
-    function createSeries(wagerData) {
-        var series = [];
-
-        var profitSeries = {
-            data: [
-                { y: 0, color: 'green', radius: 7 }
-            ]
-        };
-
-        //Create the won projection series
-        var wonProjectionSeries = {
-            color: "#12ff35",
-            data: [
-                {
-                    y: 0,
-                    x: 0
-                },
-                {
-                    y: 0 + (wagerData.wager * wagerData.payout) - wagerData.wager,
-                    x: 1
-                }
-            ]
-        };
-
-        //Create the lost projection series
-        var lostProjectionSeries = {
-            color: "#FA3C4F",
-            data: [
-                {
-                    y: 0,
-                    x: 0
-                },
-                {
-                    y: 0 - wagerData.wager,
-                    x: 1
-                }
-            ]
-        };
-
-        series.push(wonProjectionSeries);
-        series.push(lostProjectionSeries);
-        series.push(profitSeries);
-
-        return series;
-    }
-
-    function createChart(node, wagerData) {
-        return new Highcharts.Chart({
-            chart: {
-                renderTo: node,
-                type: 'line',
-                //margin: [70, 50, 60, 80], //Margin around the graph container
-                animation: {
-                    duration: 600
-                },
-                events: {
-                    redraw: function () {
-                        //alert('The chart is being redrawn');
-                    }
-                }
-                //backgroundColor: '#272B30'
-            },
-            title: {
-                text: 'Dust Dice'
-                //style: {
-                //    color: '#ffffff',
-                //    fontWeight: 'bold'
-                //}
-            },
-            subtitle: {
-                text: 'The simplest casino you could find'
-                //style: {
-                //    color: '#ffffff',
-                //    fontWeight: 'bold'
-                //}
-            },
-            xAxis: {
-                //gridLineWidth: 1,
-                //minPadding: 0.2,
-                //maxPadding: 0.2,
-                //maxZoom: 60,
-                //min: 0,
-                //max: 30,
-                //tickPixelInterval: 100,
-                //tickInterval: 1,
-                //minPadding: 0.2,
-                minRange: 30,
-                allowDecimals: false
-            },
-            yAxis: {
-                title: {
-                    text: 'Balance'
-                },
-                minPadding: 0.2,
-                maxPadding: 0.2,
-                maxZoom: 60,
-                plotLines: [{
-                    value: 0,
-                    width: 1,
-                    color: '#808080'
-                }]
-            },
-            legend: {
-                enabled: false
-            },
-            exporting: {
-                enabled: false
-            },
-            plotOptions: {
-                series: {
-                    lineWidth: 1,
-                    marker: {
-                        //radius: 8,
-                        symbol: 'circle'
-                    }
-                }
-            },
-            series: createSeries(wagerData)
-        });
-
-    }
-});
+define([    'lib/react',    'highcharts-theme',    'game-logic/engine'],function(    React,    HighCharts,    Engine){    var D = React.DOM;    var MAX_NUM_POINTS = 30;    function getBetValues() {        return Engine.getBetValues();    }    return React.createClass({        displayName: 'Graph',        chart: null,        onWagerChange: function(wagerData) {            if(!this.isMounted())                return;            var chartLength = this.chart.series[2].data.length-1;            var chartIndex = this.chart.series[2].data[this.chart.series[2].data.length - 1].x;            this.updateProjections(chartLength, chartIndex, wagerData);        },        shouldComponentUpdate: function(nextProps, nextState){            return false; //Never render        },        onNewBet: function(game, wagerData) { //The position in the array of this bet            if(!this.isMounted())                return;            //Create the new point and add it without redrawing            var newPoint;            if(game.win)                newPoint = {                    color: 'green',                    lineColor: 'green',                    y: wagerData.gameProfit,                    radius: 7                };            else                newPoint = {                    color: 'red',                    y: wagerData.gameProfit,                    radius: 7                };            var chartLength = this.chart.series[2].data.length; //The real length of the chart            var chartIndex = this.chart.series[2].data[this.chart.series[2].data.length - 1].x + 1;            this.chart.series[2].data[chartLength-1].update({ radius: 4 }, false);            this.chart.series[2].addPoint(newPoint); //The length of the graph is not updated until render time            if(chartLength > MAX_NUM_POINTS)                this.chart.series[2].data[0].remove(false);            this.updateProjections(chartLength, chartIndex, wagerData, game);        },        updateProjections: function(chartLength, chartIndex, wagerData, game) {            var wonProjection = wagerData.gameProfit + (wagerData.wager * wagerData.payout) - wagerData.wager;            var LostProjection = wagerData.gameProfit - wagerData.wager;            //if(game)            //{            //    if(game.win) {            //        this.chart.series[0].data[0].remove(false);            //        this.chart.series[0].addPoint([chartIndex + 1, wonProjection], false);            //            //        this.chart.series[1].data[0].update([chartIndex, wagerData.gameProfit], false);            //        this.chart.series[1].data[1].update([chartIndex + 1, LostProjection], false);            //    } else {            //        this.chart.series[0].data[0].update([chartIndex, wagerData.gameProfit], false);            //        this.chart.series[0].data[1].update([chartIndex + 1, wonProjection], false);            //            //        this.chart.series[1].data[0].remove(false);            //        this.chart.series[1].addPoint([chartIndex + 1, LostProjection], false);            //    }            //}            //else            //{                this.chart.series[0].data[0].update([chartIndex, wagerData.gameProfit], false);                this.chart.series[0].data[1].update([chartIndex + 1, wonProjection], false);                this.chart.series[1].data[0].update([chartIndex, wagerData.gameProfit], false);                this.chart.series[1].data[1].update([chartIndex + 1, LostProjection], false);            //}            this.chart.redraw();        },        componentDidMount: function() {            Engine.addWagerListener(this.onWagerChange);            Engine.addBetListener(this.onNewBet);            this.chart = createChart(this.refs.graphBox.getDOMNode(), Engine.getWagerValues());        },        componentWillUnmount: function() {            Engine.removeWagerListener(this.onWagerChange);            Engine.removeBetListener(this.onNewBet);            this.chart.destroy();        },        render: function() {            return D.div({ ref: 'graphBox' });        }    });    function createSeries(wagerData) {        var series = [];        var profitSeries = {            data: [                { y: 0, color: 'green', radius: 7 }            ]        };        //Create the won projection series        var wonProjectionSeries = {            color: "#12ff35",            data: [                {                    y: 0,                    x: 0                },                {                    y: 0 + (wagerData.wager * wagerData.payout) - wagerData.wager,                    x: 1                }            ]        };        //Create the lost projection series        var lostProjectionSeries = {            color: "#FA3C4F",            data: [                {                    y: 0,                    x: 0                },                {                    y: 0 - wagerData.wager,                    x: 1                }            ]        };        series.push(wonProjectionSeries);        series.push(lostProjectionSeries);        series.push(profitSeries);        return series;    }    function createChart(node, wagerData) {        return new Highcharts.Chart({            chart: {                renderTo: node,                type: 'line',                margin: [70, 50, 60, 80], //Margin around the graph container                animation: {                    duration: 400,                    easing: 'easeInOutElastic'                },                events: {                    redraw: function () {                        //alert('The chart is being redrawn');                    }                }                //backgroundColor: '#272B30'            },            plotOptions: {                series: {                    lineWidth: 1,                    marker: {                        //radius: 8,                        symbol: 'circle'                    },                    animation: {                        duration: 2000                    }                }            },            title: {                text: 'Dust Dice'                //style: {                //    color: '#ffffff',                //    fontWeight: 'bold'                //}            },            subtitle: {                text: 'The simplest casino you could find'                //style: {                //    color: '#ffffff',                //    fontWeight: 'bold'                //}            },            xAxis: {                //gridLineWidth: 1,                //minPadding: 0.2,                //maxPadding: 0.2,                //maxZoom: 60,                //min: 0,                //max: 30,                //tickPixelInterval: 100,                //tickInterval: 1,                //minPadding: 0.2,                minRange: 30,                allowDecimals: false            },            yAxis: {                title: {                    text: 'Balance'                },                minPadding: 0.2,                maxPadding: 0.2,                maxZoom: 60,                plotLines: [{                    value: 0,                    width: 1,                    color: '#808080'                }]            },            legend: {                enabled: false            },            exporting: {                enabled: false            },            series: createSeries(wagerData)        });    }});
--- scripts/config/config.js
+++ scripts/config/config.js
@@ -2,19 +2,25 @@ requirejs.config({
     baseUrl: "scripts/", //If no baseUrl is explicitly set in the configuration, the default value will be the location of the HTML page that loads require.js.
     paths: {
         highcharts: 'lib/highcharts-src',
-        'highcharts-theme': 'lib/dark-unica'
+        'highcharts-theme': 'lib/dark-unica',
+        jquery: 'lib/jquery',
+        jqueryui: 'lib/jquery-ui-1.11.2/jquery-ui.min'
     },
     shim: {
-        jquery: {
-            exports: 'jQuery'
+        //jquery: {
+        //    exports: '$'
+        //},
+        jqueryui: {
+            deps: ['jquery'],
+            exports: '$'
         },
         highcharts: {
-            deps: ['lib/jquery'],
+            deps: ['jquery'],
             exports: 'Highcharts'
 
         },
         'highcharts-theme': {
-            deps: ['highcharts'],
+            deps: ['highcharts', 'jqueryui'],
             exports: 'Highcharts'
         }
     }
