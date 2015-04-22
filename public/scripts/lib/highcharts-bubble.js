d72483ddd5495f9ca940166a41ec59ff8de2f41d
Rakesh <rakesh@moneypot.com>
2015-04-22 13:49:25 -0500

    Fix requirejs plugin not loading

--- public/scripts/lib/highcharts-bubble.js
+++ public/scripts/lib/highcharts-bubble.js
@@ -1,22 +1,19 @@
-define(function() {
-    /*global Highcharts*/
-        Highcharts.wrap(Highcharts.Pointer.prototype, 'onContainerClick', function (original, e) {
-            var pointer = this,
-                parent = pointer.chart.container.parentNode,
-                bubbleUp = true;
+Highcharts.wrap(Highcharts.Pointer.prototype, 'onContainerClick', function (original, e) {
+    var pointer = this,
+        parent = pointer.chart.container.parentNode,
+        bubbleUp = true;
 
-            console.log('onCointainerClick');
+    // Add a method to the event to allow event handlers to prevent propagation if desired
+    e.swallowByHighCharts = function () {
+        bubbleUp = false;
+    };
 
-            // Add a method to the event to allow event handlers to prevent propagation if desired
-            e.swallowByHighCharts = function() { bubbleUp = false; };
+    // Call the original event
+    original.apply(this, Array.prototype.slice.call(arguments, 1));
 
-            // Call the original event
-            original.apply(this, Array.prototype.slice.call(arguments, 1));
-
-            // Trigger the event on the container's parent (to bubble the event out of highcharts)
-            // unless the user wanted to stop it
-            if (bubbleUp && typeof parent !== 'undefined' && parent) {
-                jQuery(pointer.chart.container.parentNode).trigger(e);
-            }
-        });
+    // Trigger the event on the container's parent (to bubble the event out of highcharts)
+    // unless the user wanted to stop it
+    if (bubbleUp && typeof parent !== 'undefined' && parent) {
+        jQuery(pointer.chart.container.parentNode).trigger(e);
+    }
 });
