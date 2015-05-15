5d301320efd279e5b5fb40342650d585cfeb8122
Rakesh <rakesh@moneypot.com>
2015-05-14 19:16:19 -0500

    Add link to chat username

--- public/scripts/components/chat-message.js
+++ public/scripts/components/chat-message.js
@@ -1,7 +1,9 @@
 define([
-    'lib/react'
+    'lib/react',
+    'stores/game'
 ], function(
-    React
+    React,
+    GameStore
 ){
     var D = React.DOM;
 
@@ -12,13 +14,23 @@ define([
             message: React.PropTypes.object.isRequired
         },
 
+        _setUnsetModal: function(modal) {
+            return function(e) {
+                GameStore.setUnsetModalFocus(modal);
+                e.stopPropagation();
+            }
+        },
+
         render: function() {
             var role = (this.props.message.user.role !== 'member')?
                 D.span({ className: 'chat-msg-role ' + this.props.message.user.role }, this.props.message.user.role) : null;
             return D.div(null,
                 D.span(null,
                     role,
-                    D.b(null, this.props.message.user.uname+':\u00a0'),
+                    D.b({ className: 'chat-msg-uname', onClick: this._setUnsetModal({ name: 'STATS', username: this.props.message.user.uname }) },
+                        D.a({ href: '#' }, this.props.message.user.uname)
+                    ),
+                    D.b(null, ':\u00a0'),
                     this.props.message.text
                 )
             );
