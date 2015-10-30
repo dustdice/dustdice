fcf431cb2a432d25238d9b8b57967f4f7475f296
raybrady <ray.brady88@outlook.com>
2015-10-30 12:58:25 -0400

    Update top-bar.js

--- public/scripts/components/top-bar.js
+++ public/scripts/components/top-bar.js
@@ -72,8 +72,8 @@ define([
         },
         
         balanceTimer: setInterval(function(){
-+            Engine.refreshBalance(); // meh whatever let's try and see how it goes...
-+        }, 5000),
+            Engine.refreshBalance(); // meh whatever let's try and see how it goes...
+            }, 5000),
 
         _toggleChat: function() {
             GameStore.toggleShowChatFocus();
