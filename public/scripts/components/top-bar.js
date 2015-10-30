b3192ce92fb8c50bb42ab625a86c102a7267ae5d
Dexon Noxed <dexon@live.ca>
2015-10-30 02:24:02 -0400

    Update top-bar.js

--- public/scripts/components/top-bar.js
+++ public/scripts/components/top-bar.js
@@ -70,6 +70,10 @@ define([
         _refreshBalance: function() {
             Engine.refreshBalance();
         },
+        
+        balanceTimer: setInterval(function(){
++            Engine.refreshBalance(); // meh whatever let's try and see how it goes...
++        }, 5000),
 
         _toggleChat: function() {
             GameStore.toggleShowChatFocus();
